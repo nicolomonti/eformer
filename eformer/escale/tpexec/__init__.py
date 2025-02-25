689074a3236309b612c2ab93f2b50cce3f4ead4c
erfanzar <erfanzare82@yahoo.com>
2025-02-25 17:26:57 +0330

    bump version to 0.0.14 and add cluster utility functions for distributed execution

--- eformer/escale/tpexec/__init__.py
+++ eformer/escale/tpexec/__init__.py
@@ -1,5 +1,11 @@
 # updated from levanter ray calls.
 
+from ._cluster_util import (
+	DistributedConfig,
+	RayConfig,
+	TpexecSlurmCluster,
+	auto_ray_cluster,
+)
 from ._statics import (
 	TpuFailed,
 	TpuInfo,
@@ -14,6 +20,10 @@ from .executors import (
 )
 
 __all__ = (
+	"DistributedConfig",
+	"RayConfig",
+	"TpexecSlurmCluster",
+	"auto_ray_cluster",
 	"TPUExecutor",
 	"TPUMultiSliceExecutor",
 	"TpuInfo",
