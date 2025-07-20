a105c40d8f61acecf7bf16972ddad5a0f26fdc7c
erfanzar <erfanzare82@yahoo.com>
2025-07-20 15:22:48 +0330

    fix: Update copyright year in conf.py and clean up requirements.txt by removing unused packages

--- docs/conf.py
+++ docs/conf.py
@@ -4,35 +4,35 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 
 project = "eformer"
-copyright = "2023, Erfan Zare Chavoshi - eformer"
+copyright = "2058, Erfan Zare Chavoshi - eformer"  # noqa
 author = "Erfan Zare Chavoshi"
 
 extensions = [
-	"sphinx.ext.autodoc",
-	"sphinx.ext.autosummary",
-	"sphinx.ext.autosectionlabel",
-	"sphinx.ext.doctest",
-	"sphinx.ext.intersphinx",
-	"sphinx.ext.mathjax",
-	"sphinx.ext.napoleon",
-	"sphinx.ext.viewcode",
-	"myst_nb",
-	"sphinx_design",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "myst_nb",
+    "sphinx_design",
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 intersphinx_mapping = {
-	"jax": ("https://jax.readthedocs.io/en/latest/", None),
-	"numpy": ("https://numpy.org/doc/stable/", None),
-	"flax": ("https://flax.readthedocs.io/en/latest/", None),
+    "jax": ("https://jax.readthedocs.io/en/latest/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "flax": ("https://flax.readthedocs.io/en/latest/", None),
 }
 
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 html_css_files = [
-	"style.css",
+    "style.css",
 ]
 
 source_suffix = [".rst", ".md", ".ipynb"]
